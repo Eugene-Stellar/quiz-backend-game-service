7b830553fecbff59424e477eadebc063eb7e461c
Eugene <bielichenko777eugene@gmail.com>
2026-02-24 09:50:17 +0100

    feat: add SchedulerConfig for task scheduling with a thread pool

---
+++ src/main/java/eugenestellar/config/SchedulerConfig.java
@@ -0,0 +1,26 @@
+package eugenestellar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@EnableScheduling
+@Configuration
+public class SchedulerConfig {
+
+  @Bean
+  public TaskScheduler taskScheduler() {
+
+    ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+
+    // Pool size: 300 threads, it's enough to process a plenty
+    // of rooms, since timerExpired() executes swiftly
+    scheduler.setPoolSize(300);
+
+    scheduler.setThreadNamePrefix("GameTimer-"); // for debugging
+    scheduler.initialize();
+    return scheduler;
+  }
+}
